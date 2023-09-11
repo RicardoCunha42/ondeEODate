a6ab52511f5eb564c4cb7cf19e741866c9f63325
RicardoCunha42 <ricardotoms42@gmail.com>
2023-09-11 12:33:16 -0300

    schema alteration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,7 @@ create table IF NOT EXISTS bares(
     id serial primary key,
     nome varchar(255) not null,
     gasto varchar(255) not null,
+    clima varchar(255) not null,
     insta varchar(255) not null,
     obs varchar(255)
 );
@@ -12,6 +13,7 @@ create table IF NOT EXISTS cafes (
     id serial primary key,
     nome varchar(255) not null,
     gasto varchar(255) not null,
+    clima varchar(255) not null,
     insta varchar(255) not null,
     obs varchar(255)
 );
