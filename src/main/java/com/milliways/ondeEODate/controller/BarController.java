a0d3d472877198cc388f13767b50c7217b2206b4
RicardoCunha42 <ricardotoms42@gmail.com>
2023-09-11 14:02:49 -0300

    crossorigins update

--- src/main/java/com/milliways/ondeEODate/controller/BarController.java
+++ src/main/java/com/milliways/ondeEODate/controller/BarController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.milliways.ondeEODate.model.Bar;
 import com.milliways.ondeEODate.service.BarService;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(originPatterns = {"https://onde-e-o-date-front.vercel.app/", "http://localhost:4200/"})
 @RestController
 @RequestMapping("bares")
 public class BarController {
