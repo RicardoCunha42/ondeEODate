2ecb7c57e2db9cdd8b626c6d017ff9ab7be9b9c6
RicardoCunha42 <ricardotoms42@gmail.com>
2023-09-11 13:51:46 -0300

    import clean

--- src/main/java/com/milliways/ondeEODate/model/Lugar.java
+++ src/main/java/com/milliways/ondeEODate/model/Lugar.java
@@ -1,11 +1,8 @@
 package com.milliways.ondeEODate.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
